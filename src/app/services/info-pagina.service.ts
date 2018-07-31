0ee159239d7930c7e51242dd52cc2d3fa07d0238
PC-RYZEN-CGPICA\PcCom <cgpicaporte@hotmail.com>
2018-07-31 04:13:34 +0200

    Fin Sección 6: Preparar Firebase con nuestros productos

--- src/app/services/info-pagina.service.ts
+++ src/app/services/info-pagina.service.ts
@@ -9,21 +9,48 @@ export class InfoPaginaService {
 
   info: InfoPagina = {};
   cargada = false;
+  equipo: any[] = [];
 
   constructor(private http: HttpClient ) {
 
     // console.log('Sevicio de InfoPagina cargado');
 
     // Leer el archivo JSON assets/data/data-pagina.json
+    // this.http.get('assets/data/data-pagina.json')
+    //     .subscribe( (resp: InfoPagina) => {
+
+    //       this.cargada = true;
+    //       this.info = resp;
+    //       console.log( resp['twitter'] );
+    //       console.log( resp );
+
+    //     });
+    this.cargarInfo();
+    this.cargarEquipo();
+
+   }
+
+   private cargarInfo() {
     this.http.get('assets/data/data-pagina.json')
-        .subscribe( (resp: InfoPagina) => {
+    .subscribe( (resp: InfoPagina) => {
 
-          this.cargada = true;
-          this.info = resp;
-          console.log( resp['twitter'] );
-          console.log( resp );
+      //this.cargada = true;
+      this.info = resp;
+      //console.log( resp['twitter'] );
+      //console.log( resp );
 
-        });
+    });
 
+   }
+
+   private cargarEquipo() {
+    this.http.get('https://cgpicaporte-portafolio-a-f1366.firebaseio.com/equipo.json')
+    .subscribe( (resp: any[] ) => {
+      this.equipo = resp;
+      this.cargada = true;
+      //console.log( resp['0'].nombre );
+      //console.log( resp['0']['twitter'] );
+      //console.log( resp );
+    });
    }
 }
